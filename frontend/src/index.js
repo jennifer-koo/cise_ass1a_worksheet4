38ac2c3f8c7d9ace8469180b39f0e59d03893fee
Jennifer Koo <rzq8568@autuni.ac.nz>
2021-08-16 08:48:46 +1200

    created main site

---
+++ frontend/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../../../cise_ass1a_worksheet2/frontend/src/App';
+import Main from "./App";
+
+ReactDOM.render(  
+  <App/>,
+  document.getElementByld("root")
+);
